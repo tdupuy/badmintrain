ba9c8f34a35789be9a27d81e336114ad6d1fb273
Nesquick <dupuythibaut@yahoo.fr>
2022-09-30 20:53:13 +0200

    Intégration du formulaire

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+var_dump($_POST);
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -8,11 +11,47 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  <form action="#" method="post">
+    <div class="form-group row">
+        <label for="nb_joueurs" class="col-4 col-form-label">Nombre de joueurs</label> 
+        <div class="col-8">
+        <div class="input-group">
+            <div class="input-group-prepend">
+            <div class="input-group-text">
+                <i class="fa fa-users"></i>
+            </div>
+            </div> 
+            <input id="nb_joueurs" name="nb_joueurs" type="text" class="form-control" required="required">
+        </div>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="nb_terrains" class="col-4 col-form-label">Nombre de terrains</label> 
+        <div class="col-8">
+        <select id="nb_terrains" name="nb_terrains" class="custom-select" required="required">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+        </select>
+        </div>
+    </div> 
+    <div class="form-group row">
+        <div class="offset-4 col-8">
+        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+    </form>
 
+    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
