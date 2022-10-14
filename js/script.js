85b44f897d3ec5ee632a1e0e424c5ba7e0ccc7f4
Nesquick <dupuythibaut@yahoo.fr>
2022-10-15 00:20:11 +0200

    Gestion du slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
 $( document ).ready(function() {
     $('.terrain_1').siblings('.terrain').addClass('d-none');
+    $('.controls .control').click(function(){
+        let direction = '';
+        if($(this).hasClass('control-prev')){
+            direction = 'prev';
+        }else{
+            direction = 'next';
+        }
+        $('.terrain').addClass('d-none');
+        let terrain = $(this).parents('.terrain');
+        $.each(terrain.attr('class').split(' '),function(index,value){
+            if(value.indexOf('terrain_') >= 0){
+                let index = value.split('_')[1];
+                if(direction == 'prev'){
+                    if(terrain.prev('.terrain').length == 0){
+                        index = $('.terrain').length;
+                    }else{
+                        index--;
+                    } 
+                }else{
+                    if(terrain.next('.terrain').length == 0){
+                        index = 1;
+                    }else{
+                        index++;
+                    }
+                }
+                $('.terrain_' + index ).removeClass('d-none');
+            }
+        });
+        
+    });
 });
